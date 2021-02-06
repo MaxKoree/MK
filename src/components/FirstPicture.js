15433c2139d27df30dc6f110eb7e0d8a54c29a26
MyzX <maxkoree@yahoo.de>
2021-02-06 12:45:37 +0100

    7.8

--- src/components/FirstPicture.js
+++ src/components/FirstPicture.js
@@ -44,7 +44,7 @@ render() {
     	    	    	    	
     	    	    	<img src={FirstPicture} alt="Me :)" id="firstPicture2"></img>
     	    	    	    	<div className="textBackground"></div>
-    			    	<p id="p">Hey, i&#39;m Max Koree. 18 years old. I live in Amsterdam. I&#39;m working towards the full stack developer title.
+    			    	<p id="p">Hey, i&#39;m Max Koree. i&#39;m 18 years old, live in Amsterdam. I&#39;m working towards the full stack developer title.
         		    	I started studying computer science when I was 15 years old. I&#39;m a student at the MBO college Zuid-Oost in Amsterdam.
         		    	I have learned a lot since, I had an intership at ABN AMRO Bank N.V., IT department (Head office).
         		    	Here I started really getting into programming.
@@ -68,23 +68,23 @@ render() {
     	     	    	<h1 className="aboutmeHeader">About me</h1>
     	     	    	<img src={FirstPicture} alt="Me :)" id="firstPicture"></img>
     	     	    	<div className="textBackground"></div>
-    	     		    	<p>Hey, i&#39;m Max Koree. 18 years old. I live in Amsterdam. I&#39;m working towards the full stack developer title.
+    	     		    	<p>Hey, i&#39;m Max Koree. 18 years old. I live in Amsterdam. I&#39;m a full stack developer.
     	     		    	I started studying computer science when I was 15 years old. I&#39;m a student at the MBO college Zuid-Oost in Amsterdam.
-    	     		    	I have learned a lot since, I had an intership at ABN AMRO Bank N.V., IT department (Head office).
+    	     		    	I&#39;ve learned a lot since, I had an intership at ABN AMRO Bank N.V., IT department (Head office).
     	     		    	Here I started really getting into programming.
     	     		    	After the internship I followed the  
     	     		    	<a href="https://www.udemy.com/course/the-complete-web-developer-zero-to-mastery/" target="_blank" rel="noreferrer"> ZeroToMastery </a> 
-    	     		    online course. <br></br><br></br> I&#39;ve written code in the following languages.<br></br><br></br>
+    	     		    online class. <br></br><br></br> I&#39;ve been writing code in the following languages.<br></br><br></br>
     	     		    	- HTML5<br></br>
     	     		    	- CSS<br></br>
+							- JavaScript<br></br>
     	     		    	- Bootstrap<br></br>
     	     		    	- jQuery<br></br>
-    	     		    	- JavaScript<br></br>
+    	     		    	<br></br>
     	     		    	- React<br></br>
     	     		    	- Node.js<br></br>
-    	     		    	- PHP<br></br>
     	     		    	- Java<br></br><br></br>
-    	     		    	I&#39;m also very motivated to learn a lot more :)</p> 
+    	     		    	Feel free to contact me if there is anything you&#39;d like to ask.</p> 
     	    	    	</div>
     	    	    	)}  	    	
 
